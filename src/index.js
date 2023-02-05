578641ea4cd81f72999dbb6dd6de1bd8522378b2
Anna Petras <apetras2017@gmail.com>
2023-02-05 18:35:17 +0100

    Fix Linter Errors for CSS and JavaScript

--- src/index.js
+++ src/index.js
@@ -1,49 +1,49 @@
 import './style.css';
 
 document.querySelectorAll('.carousel').forEach((carousel) => {
-    const items = carousel.querySelectorAll('.carousel__item');
-    const buttonsHtml = Array.from(items, () => '<span class="carousel__button"></span>');
+  const items = carousel.querySelectorAll('.carousel__item');
+  const buttonsHtml = Array.from(items, () => '<span class="carousel__button"></span>');
 
-    carousel.insertAdjacentHTML(
-        'beforeend',
-        `
+  carousel.insertAdjacentHTML(
+    'beforeend',
+    `
       <div class="carousel__nav">
         ${buttonsHtml.join('')}
       </div>
     `,
-    );
+  );
 
-    const buttons = carousel.querySelectorAll('.carousel__button');
-    const audios = carousel.querySelectorAll('audio');
-    let selectedIndex = 0;
+  const buttons = carousel.querySelectorAll('.carousel__button');
+  const audios = carousel.querySelectorAll('audio');
+  let selectedIndex = 0;
 
-    items[selectedIndex].classList.add('carousel__item--selected');
-    buttons[selectedIndex].classList.add('carousel__button--selected');
-    audios[selectedIndex].play();
+  items[selectedIndex].classList.add('carousel__item--selected');
+  buttons[selectedIndex].classList.add('carousel__button--selected');
+  audios[selectedIndex].play();
 
-    audios.forEach((audio, index) => {
-        audio.addEventListener('ended', () => {
-            audio.pause();
-            buttons[selectedIndex].classList.remove('carousel__button--selected');
-            items[selectedIndex].classList.remove('carousel__item--selected');
-            selectedIndex = (selectedIndex + 1) % items.length;
-            buttons[selectedIndex].classList.add('carousel__button--selected');
-            items[selectedIndex].classList.add('carousel__item--selected');
-            audio.currentTime = 0;
-            audio.play();
-        });
+  audios.forEach((audio, index) => {
+    audio.addEventListener('ended', () => {
+      audio.pause();
+      buttons[selectedIndex].classList.remove('carousel__button--selected');
+      items[selectedIndex].classList.remove('carousel__item--selected');
+      selectedIndex = (selectedIndex + 1) % items.length;
+      buttons[selectedIndex].classList.add('carousel__button--selected');
+      items[selectedIndex].classList.add('carousel__item--selected');
+      audio.currentTime = 0;
+      audio.play();
     });
+  });
 
-    buttons.forEach((button, index) => {
-        button.addEventListener('click', () => {
-            audios[selectedIndex].pause();
-            buttons[selectedIndex].classList.remove('carousel__button--selected');
-            items[selectedIndex].classList.remove('carousel__item--selected');
-            selectedIndex = index;
-            buttons[selectedIndex].classList.add('carousel__button--selected');
-            items[selectedIndex].classList.add('carousel__item--selected');
-            audios[selectedIndex].currentTime = 0;
-            audios[selectedIndex].play();
-        });
+  buttons.forEach((button, index) => {
+    button.addEventListener('click', () => {
+      audios[selectedIndex].pause();
+      buttons[selectedIndex].classList.remove('carousel__button--selected');
+      items[selectedIndex].classList.remove('carousel__item--selected');
+      selectedIndex = index;
+      buttons[selectedIndex].classList.add('carousel__button--selected');
+      items[selectedIndex].classList.add('carousel__item--selected');
+      audios[selectedIndex].currentTime = 0;
+      audios[selectedIndex].play();
     });
+  });
 });
